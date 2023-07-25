d869f768563b8e9ceeb50a5c202de574d21012fd
Junaid Aslam <junaidaslam.muet@gmail.com>
2023-07-25 08:19:13 +0500

    Add files via upload

---
+++ Week-02/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+ 
+// State:
+// Behaviour of Action
+// Values of Variables at any point is State:
+
+ 
+
+void main() {
+  runApp(const MyApp());
+}
+
+ 
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHomePage(title: 'Hello Futter Developers!'));
+  }
+}
+
+ 
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({super.key, this.title = "Good Afternoon"});
+  String title;
+
+ 
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+ 
+
+class _MyHomePageState extends State<MyHomePage> {
+  int counter = 0;
+  int times = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      floatingActionButton: Row(
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              print("Chat Button Pressed: $counter times");
+              setState(() {
+                widget.title = "New Title $counter";
+                counter++;
+              });
+            },
+            child: const Icon(Icons.chat),
+          ),
+          FloatingActionButton(
+            onPressed: () { setState(() {
+              times++;
+            });
+            widget.title = "Variable $times";},
+            child: Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: () { setState(() {
+              times--;
+            });
+            widget.title = "Variable $times";},
+            child: Icon(Icons.subtitles_rounded),
+          ),
+        ],
+      ),
+    );
+  }
+}
