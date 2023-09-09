d23e02460deca871a57601de9384bb9ba112e5be
Junaid Aslam <junaidaslam.muet@gmail.com>
2023-09-09 14:19:16 +0500

    Add files via upload

---
+++ Week-08/main.dart
@@ -0,0 +1,83 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'User.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter REST apis Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter REST apis Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  dynamic _users = [];
+  bool loading = false;
+
+  Future<String> _getUsers() async {
+    setState(() => loading = true);
+    var response = await http.get(Uri.parse("https://jsonplaceholder.typicode.com/users"));
+    setState(() {
+      _users = json.decode(response.body.toString());
+      loading = false;
+    });
+    return "Success";
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getUsers();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("REST API's"),
+      ),
+      body: Column(
+        children: [
+          loading
+              ? Center(
+                  child: CircularProgressIndicator(),
+                )
+              : Expanded(
+                  child: ListView.builder(
+                    itemCount: _users.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading: Text(_users[index]["id"].toString()),
+                        title: Text(_users[index]["name"]),
+                        subtitle: Text(_users[index]["email"]),
+                        trailing: Text(_users[index]["website"]),
+                      );
+                    },
+                  ),
+                ),
+        ],
+      ),
+    );
+  }
+}
