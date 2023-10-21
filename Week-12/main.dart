e4474e95dfdacc3680a6e11eda10732b741c0f99
Junaid Aslam <junaidaslam.muet@gmail.com>
2023-10-21 12:05:32 +0500

    Firebase (CloudFirestore)

--- Week-12/main.dart
+++ Week-12/main.dart
@@ -3,7 +3,7 @@ import 'package:firebase_lab/auth_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'firebase_options.dart';
+import 'firebase_options.dart'; 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
